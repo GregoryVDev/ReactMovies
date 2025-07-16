24507b717974c09d6918a03f870af11bab75b13b
GregoryVDev <arexo@hotmail.fr>
2025-07-16 18:20:25 +0200

    Added .then and res.json to transform the response into a JSON form. Recovered the list of films and saved it in the UseState. Displayed with .map each title and added a unique key for each film

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,17 @@ export const Card = () => {
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR`
-    );
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      {movies.map((film) => (
+        <article key={film.id} className="article-movie">
+          <h3>{film.original_title}</h3>
+        </article>
+      ))}
+    </div>
+  );
 };
