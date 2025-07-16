fb04c08b24da8af6eeda79343271fc4694a49098
GregoryVDev <arexo@hotmail.fr>
2025-07-16 15:16:00 +0200

    Removed StrictMode, createRoot import. Added React  import.  Added BrowserRouter, Routes and Route. Created Navigation and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { Header } from "./components/Header";
 import "./css/App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
       <Header />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./css/index.css";
 import { App } from "./App.jsx";
+import React from "react";
+import "./css/index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
