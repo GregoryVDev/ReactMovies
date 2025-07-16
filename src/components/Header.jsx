9ed9268a9be24c06ed33d51d55a87ece94763a5c
GregoryVDev <arexo@hotmail.fr>
2025-07-16 15:24:12 +0200

    Added header with a Navigation in header.jsx file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { NavLink } from "react-router-dom";
 import "../css/header.css";
 
 export const Header = () => {
   return (
-    <div>
-      <h1>React Movies</h1>
-    </div>
+    <header>
+      <div className="navigation">
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink
+            to="/like"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Coup de coeur</li>
+          </NavLink>
+          <h1>React Movies</h1>
+        </ul>
+      </div>
+    </header>
   );
 };
