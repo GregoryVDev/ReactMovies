b71e5a1958bdfe5aa1c3fb25f488a652f2fa1817
GregoryVDev <arexo@hotmail.fr>
2025-07-24 21:10:31 +0200

    Removed axios and useEffect (already in Card component)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,4 @@
 export const Search = () => {
-  useEffect(() => {
-    axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR`
-    );
-  }, []);
-
   return (
     <div className="form-component">
       <div className="container-search">
