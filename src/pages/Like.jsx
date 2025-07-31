6b75a1225d82551191cfa6e2ed5a596f59e468b6
GregoryVDev <arexo@hotmail.fr>
2025-07-31 15:17:54 +0200

    Asynchronous retrieval of film data via axios, storage in movieArray and updating of the displayed list + added comments for explain

--- src/pages/Like.jsx
+++ src/pages/Like.jsx
@@ -6,6 +6,7 @@ export const Like = () => {
   const [listData, setListData] = useState([]);
 
   useEffect(() => {
+    let movieArray = [];
     let moviesId = window.localStorage.movie
       ? // Permet avec .split de transformer la chaine en tableau
         window.localStorage.movie.split(",")
@@ -16,8 +17,17 @@ export const Like = () => {
         .get(
           `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`
         )
-        .then((res) => console.log(res.data));
+        .then((res) =>
+          // On ajoute les données dans movieArray
+          movieArray.push(res.data)
+        )
+        .then(() =>
+          // On met à jour la liste des films affichée
+          setListData(movieArray)
+        );
     }
+    // Mise à jour finale de la liste
+    setListData(movieArray);
   }, []);
 
   return (
