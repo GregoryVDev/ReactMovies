a044d061495b939869208a8a81b345f5d2789ab0
GregoryVDev <arexo@hotmail.fr>
2025-07-31 14:00:58 +0200

    Added useState and useEffect to load film IDs's from localStorage

--- src/pages/Like.jsx
+++ src/pages/Like.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
 
 export const Like = () => {
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies ? window.localStorage.movies : [];
+  }, []);
+
   return (
     <div className="like-page">
       <Header />
